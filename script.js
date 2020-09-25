9170743b9f799e0f53c342389b775de5b4b0262d
github wubuchako <jfdev12@gmail.com>
2020-09-25 15:57:12 +0900

    prototype

--- script.js
+++ script.js
@@ -14,20 +14,19 @@ var Person = function(name, yearOfBirth, job){
 
 Person.prototype.calculateAge = function(){
     console.log(2016 - this.yearOfBirth);
+};
 
-    
+Person.prototype.lastName = 'Smith';
+
    //instantiation 
 var john = new Person('John', 1990, 'teacher');
-
-john.calculateAge();
-
 var jane = new Person('Jane', 1969, 'designer');
 var mark = new Person('Mark', 1948, 'retired');
 
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
 
-
-
-
-
-
-
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
